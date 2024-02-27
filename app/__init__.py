a97152ea97c0a69693072130c36dcb10057c91c7
Muliro <mulirokhaemba@gmail.com>
2024-02-27 20:34:40 +0300

    newbranch

--- app/__init__.py
+++ app/__init__.py
@@ -3,5 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 app.config.from_object('config')
 
+from app import views
+
 
 # Now we can access the configuration variables via app.config["VAR_NAME"].
