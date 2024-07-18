1830dcafb1cd94f94b1c1b887b9f44548eaafcf4
Muliro1 <mulirokhaemba@gmail.com>
2024-07-18 10:25:35 +0300

    functioning-locally

--- app/__init__.py
+++ app/__init__.py
@@ -3,18 +3,22 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import mysql.connector
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
-mydb = mysql.connector.connect(host='localhost', user='muliro', passwd='nihilpraeteroptimum')
-cur = mydb.cursor()
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://muliro:nihilpraeteroptimum@localhost/fitness'
+#mydb = mysql.connector.connect(host='localhost', user='muliro', passwd='nihilpraeteroptimum')
+#cur = mydb.cursor()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///fitness.db'
 app.config['SECRET_KEY'] = 'relapse'
 app.config.from_object('config')
 db = SQLAlchemy(app)
+with app.app_context():
+    db.create_all()
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'users.login'
+migrate = Migrate(app, db)
 
 from app import  models
 
